9c8ad39355a893fe6187b8ca05255e87fab27ffa
Philipp-09 <hosekphilipp@gmail.com>
2023-12-04 11:25:34 +0100

    SPMB-2 Meta-Daten Song

---
+++ song.h
@@ -0,0 +1,12 @@
+#include <string>
+#include "album.h"
+
+class Song{
+    public:
+    std::string songtitel;
+    std::string albumname;
+    std::string kuenstlername;
+    int erscheinungsjahr;
+    int songlaenge;
+    std::string genre;
+};
