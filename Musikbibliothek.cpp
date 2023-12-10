61433c7111094de8e3551ec2b7ad7696efc68a73
Philipp-09 <hosekphilipp@gmail.com>
2023-12-10 10:14:31 +0100

    SPMB-1 Song hinzufügen

--- Musikbibliothek.cpp
+++ Musikbibliothek.cpp
@@ -24,5 +24,25 @@ void album_hinzufuegen(std::string albumname,std::string kuenstlername,Song *son
     alben[anzahl_alben]=album; //fügt das neue Album in die Albumliste hinzu
     anzahl_alben++; //Anzahl der Alben erhöht sich um eins 
 }
+//Funktion bekommt neue Meta-Daten
+void song_hinzufuegen(std::string songtitel, Album album,std::string kuenstlername,int erscheinungsjahr,int songlaenge,std::string genre){
+    if (anzahl_songs%100==0){ //falls Songliste voll ist 
+        Song* neue_songs=(Song*) malloc((anzahl_songs+100)*sizeof(Song)); //neue Songliste um 100 länger
+        for(int i=0; i<anzahl_songs; i++){ 
+            neue_songs[i]=songs[i]; //kopiert alte Songliste in die neue 
+        }
+        free(songs); //Löscht alte Songliste 
+        songs=neue_songs; // schiebt die neue in die alte 
+    }
+    Song song; //neuer Song mit neuen Meta-Daten
+    song.songtitel=songtitel;
+    song.album=album;
+    song.kuenstlername=kuenstlername;
+    song.erscheinungsjahr=erscheinungsjahr;
+    song.songlaenge=songlaenge;
+    song.genre=genre;
+    songs[anzahl_songs]=song; //fügt den neuen Song in die Songliste hinzu
+    anzahl_songs++; //Anzahl der Songs erhöht sich um eins 
+}
 
 
