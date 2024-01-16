3d2226b96d8519dc05955094d8c1c00f4249f703
Philipp-09 <hosekphilipp@gmail.com>
2024-01-16 18:54:49 +0100

    verbesserung Röhricht

--- song.cpp
+++ song.cpp
@@ -2,6 +2,6 @@
 std::string Song::to_string_csv(){
         return songtitel+";"+kuenstlername+";"+std::to_string(erscheinungsjahr)+";"+std::to_string(songlaenge)+";"+genre;
 }
-std::string Song::to_string_csv(){
+std::string Song::to_string_ausgabe(){
         return songtitel+" ("+album.albumname+") - "+kuenstlername+" ("+std::to_string(erscheinungsjahr)+", "+genre+"): "+std::to_string(songlaenge)+" Sekunden Länge;\n";
 }
